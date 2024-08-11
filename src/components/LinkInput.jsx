d6c58fb78713d303920740754e497f412a4d4a6c
JesusVA760 <tonio4340406@gmail.com>
2024-08-11 15:55:28 -0700

    error

--- src/components/LinkInput.jsx
+++ src/components/LinkInput.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LinkInput.css";
 
 const LinkInput = () => {
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="linkInput-wrapper">
       <div className="linkInput-container">
         <div className="link-content">
-          <input type="url" placeholder="Shorten a link here..." />
+          <div className="link-content-input">
+            <input
+              type="url"
+              onChange={() => setIsEmpty(true)}
+              placeholder="Shorten a link here..."
+            />
+            {!isEmpty ? <p className="add-link">Please add a link!</p> : ""}
+          </div>
+          <div className="div"></div>
           <button className="shorten-link-button">Shorten it!</button>
         </div>
       </div>
