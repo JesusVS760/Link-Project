3c18607697d4be9aa819459aeb30cb8a30cebdec
JesusVA760 <tonio4340406@gmail.com>
2024-08-14 15:56:18 -0700

    useMediaQuery update

--- src/hooks/useMediaQuery.js
+++ src/hooks/useMediaQuery.js
@@ -6,12 +6,15 @@ const useMediaQuery = () => {
   useEffect(() => {
     const handleResize = () => setIsMediaQuery(window.innerWidth);
 
+    // listening to the
     window.addEventListener("resize", handleResize);
 
+    // clean up function to remove all unintended side effects
+    //listen to an event of resize, than execute function
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  return isMediaQuery;
+  return isMediaQuery; // returning the width of screen
 };
 
 export default useMediaQuery;
